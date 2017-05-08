9a3a1ccdef65ee9a633053e82d7e48fcf741c662
beepscore <support@beepscore.com>
2017-05-07 21:13:21 -0700

    In requirements comment out sklearn. Add comments

--- machine_learning_tutorial/stock.py
+++ machine_learning_tutorial/stock.py
@@ -4,6 +4,9 @@ import math
 import numpy as np
 #import pandas as pd
 import quandl
+
+# sklearn represents scikit-learn
+# http://stackoverflow.com/questions/38733220/difference-between-scikit-learn-and-sklearn
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 
