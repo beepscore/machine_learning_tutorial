ddfe5ef63284fd909f821c26a07ebbc88473424f
beepscore <support@beepscore.com>
2017-05-28 22:21:56 -0700

    In cancer.py add a second classifier that uses svm support vector machine

--- machine_learning_tutorial/cancer.py
+++ machine_learning_tutorial/cancer.py
@@ -5,12 +5,17 @@ import pandas as pd
 
 # sklearn represents scikit-learn
 # http://stackoverflow.com/questions/38733220/difference-between-scikit-learn-and-sklearn
-from sklearn import preprocessing, model_selection, neighbors
+from sklearn import preprocessing, model_selection
 
-"""
-k nearest neighbors
-https://pythonprogramming.net/k-nearest-neighbors-application-machine-learning-tutorial/?completed=/k-nearest-neighbors-intro-machine-learning-tutorial/
-"""
+# k nearest neighbors
+# https://pythonprogramming.net/k-nearest-neighbors-application-machine-learning-tutorial/?completed=/k-nearest-neighbors-intro-machine-learning-tutorial/
+from sklearn import neighbors
+
+# svm support vector machine
+# https://pythonprogramming.net/support-vector-machine-intro-machine-learning-tutorial/?completed=/final-thoughts-knn-machine-learning-tutorial
+# http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+from sklearn import svm
+
 
 input_directory = "../data/input/"
 input_file_name = "breast-cancer-wisconsin-data.csv"
@@ -37,17 +42,23 @@ y = np.array(df['class'])
 # http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-# classifier
+# k nearest neighbor classifier
 # look at class of k nearest neighbors to predict item class
 clf = neighbors.KNeighborsClassifier()
 
-# train the classifier
+# support vector classifier
+clf_svm = svm.SVC()
+
+# train the classifiers
 clf.fit(X_train, y_train)
+clf_svm.fit(X_train, y_train)
 
 # test
 accuracy = clf.score(X_test, y_test)
+accuracy_svm = clf_svm.score(X_test, y_test)
 # expect accuracy ~ 0.95
 print('accuracy', accuracy)
+print('accuracy_svm', accuracy_svm)
 
 # fake data, doesn't match any rows in data set
 # one row
@@ -63,6 +74,8 @@ example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 1, 1, 2, 3, 
 example_measures = example_measures.reshape(len(example_measures), -1)
 
 prediction = clf.predict(example_measures)
+prediction_svm = clf_svm.predict(example_measures)
 # one row prints prediction [2]
 # two rows prints prediction [2 2]
 print('prediction', prediction)
+print('prediction_svm', prediction_svm)
